a259e23480444835dc854258dc1da2cbb1b77166
GeorgeDynamite <64864098+GeorgeDynamite@users.noreply.github.com>
2020-07-18 12:55:39 -0500

    Add files via upload
    add a JS

---
+++ recipieJS.js
@@ -0,0 +1,10 @@
+var queryURL = "https://rapidapi.com/mycookbook/api/mycookbook-io1"
+
+// spoonacular API 09ec552e04956556a7e5b7ce278895625f8f8351
+//spoonacular PIN 40972
+
+
+//define variables
+//link ajax/api
+//search functionality
+//button functionality
