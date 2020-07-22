6d400f009d4cf85ed40502e8ddc8c5d416b3bc46
GeorgeDynamite <64864098+GeorgeDynamite@users.noreply.github.com>
2020-07-21 19:02:58 -0500

    Add files via upload

--- recipieJS.js
+++ recipieJS.js
@@ -6,15 +6,16 @@ $(".searchBtn").on("click", function () {
     var numResults = $("#numRecipes").val();
     const apiKey = "&apiKey=b159b41ff9234e75bdc4a617a3838577";
     var queryUrl = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredient + "&number=" + numResults + apiKey;
+    console.log(queryUrl)
     //https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples&number=2&apiKey=b159b41ff9234e75bdc4a617a3838577
-    console.log(queryUrl);
     $.ajax({
         url: queryUrl,
         method: "GET"
     })
         .then(function (response) {
-            event.preventDefault
             console.log(response);
+            event.preventDefault();
+            
 
         });
 });
