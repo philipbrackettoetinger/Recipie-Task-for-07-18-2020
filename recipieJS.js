8aa669ec92e105be3906c111bcb68525ab57e2b5
GeorgeDynamite <64864098+GeorgeDynamite@users.noreply.github.com>
2020-07-21 19:49:30 -0500

    Add files via upload
    added support for multiple ingredients

--- recipieJS.js
+++ recipieJS.js
@@ -1,11 +1,13 @@
 
 
-$(".searchBtn").on("click", function () {
+$(".submit").on("click", function () {
     event.preventDefault()
-    var ingredient = $("#searchTerm").val();
+    var ingredient = $("#ingredient").val();
+    var ingredientTwo = $("#ingredientTwo").val();
+    var ingredientThree = $("#ingredientThree").val();
     var numResults = $("#numRecipes").val();
     const apiKey = "&apiKey=b159b41ff9234e75bdc4a617a3838577";
-    var queryUrl = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredient + "&number=" + numResults + apiKey;
+    var queryUrl = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredient + ",+" + ingredientTwo + ",+" + ingredientThree + "&number=" + numResults + apiKey;
     console.log(queryUrl)
     //https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples&number=2&apiKey=b159b41ff9234e75bdc4a617a3838577
     $.ajax({
@@ -14,11 +16,21 @@ $(".searchBtn").on("click", function () {
     })
         .then(function (response) {
             console.log(response);
+            console.log(response[0].image)
+            console.log()
             event.preventDefault();
             
-
+            createCards(response)
+            createRecipe(response)
         });
 });
+
+function createCards(){
+
+}
+function createRecipe(){
+
+}
 
 //define variables
 //link ajax/api
