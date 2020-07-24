2bee447109700b2f144c06c121553b7c5fa6bdb6
GeorgeDynamite <64864098+GeorgeDynamite@users.noreply.github.com>
2020-07-23 19:44:33 -0500

    Add files via upload

--- recipieJS.js
+++ recipieJS.js
@@ -2,12 +2,12 @@
 
 $(".submit").on("click", function () {
     event.preventDefault()
-    var ingredient = $("#ingredient").val();
-    var ingredientTwo = $("#ingredientTwo").val();
+    var ingredient = $("#ingredient").val() +",";
+    var ingredientTwo = $("#ingredientTwo").val() + ",";
     var ingredientThree = $("#ingredientThree").val();
     var numResults = $("#numRecipes").val();
     const apiKey = "&apiKey=b159b41ff9234e75bdc4a617a3838577";
-    var queryUrl = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredient + ",+" + ingredientTwo + ",+" + ingredientThree + "&number=" + numResults + apiKey;
+    var queryUrl = "https://api.spoonacular.com/recipes/complexSearch?query=" + ingredient + ingredientTwo + ingredientThree +  "&addRecipeInformation=true&number=" + numResults + apiKey;
     console.log(queryUrl)
     $.ajax({
         url: queryUrl,
@@ -23,9 +23,14 @@ $(".submit").on("click", function () {
 
 
 function createRecipe(response){
-    var image = $("<img>").attr("src", "https://spoonacular.com/recipeImages/" + response[0].id +"-312x231.jpg")
+    var image = $("<img>").attr("src", "https://spoonacular.com/recipeImages/" + response.results[0].id +"-312x231.jpg")
     console.log(image)
-    $(".panel-body").append(image)
+    var title = $("<h4>").text(response.results[0].title)
+    var recipeURL = $("<a>").text(response.results[0].sourceURL)
+    console.log(recipeURL)
+    $(".recipeCard").append(recipeURL)
+    $(".recipeCard").append(image)
+    $(".recipeCard").prepend(title)
 
 }
 
